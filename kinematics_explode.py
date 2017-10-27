4e20b417dcf2f7f4859d2c2a215e869e317ff497
Hyobin You <hy002014@mymail.pomona.edu>
2017-10-27 14:03:59 -0700

    parse to x_test and y_test

---
+++ kinematics_explode.py
@@ -0,0 +1,12 @@
+"""
+create X_test.txt and y_test.txt for JIGSAW kinematics
+
+
+make changes to this code to correctly point to the directory containing the completely compiled JIGSAW kinematics data
+(product of kinematics_collapse)
+
+"""
+
+import numpy as np
+import os
+
